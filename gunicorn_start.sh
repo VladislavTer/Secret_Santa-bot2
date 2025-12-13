6a683f09f017651cf7d7058459a53715bae0d317
VladislavTer <167648741+VladislavTer@users.noreply.github.com>
2025-12-13 13:14:48 +0300

    Удаляю лишние стартовые скрипты

--- gunicorn_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🚀 Запуск через gunicorn..."
-gunicorn main:app \
-  --bind 0.0.0.0:$PORT \
-  --workers 1 \
-  --timeout 120 \
-  --access-logfile - \
-  --error-logfile - \
-  --preload
